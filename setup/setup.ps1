c6daba893a20668d51ad8d6f8329d7c0513cfc06
trnktms <tarnoktomi@gmail.com>
2017-12-07 23:01:24 +0100

    Add new logo

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -50,9 +50,11 @@ function Info($message) {
 
 function Logo() {
     $color = "darkmagenta"
-    Write-Host("==============") -Foreground $color
-    Write-Host("HELIX SKELETON") -Foreground $color
-    Write-Host("==============") -Foreground $color
+    Write-Host("   __ ________   _____  __  ______ ________   ______________  _  __") -Foreground $color
+    Write-Host("  / // / __/ /  /  _/ |/_/ / __/ //_/ __/ /  / __/_  __/ __ \/ |/ /") -Foreground $color
+    Write-Host(" / _  / _// /___/ /_>  <  _\ \/ ,< / _// /__/ _/  / / / /_/ /    /") -Foreground $color 
+    Write-Host("/_//_/___/____/___/_/|_| /___/_/|_/___/____/___/ /_/  \____/_/|_/") -Foreground $color
+    Write-Host("")
 }
 
 # settings
