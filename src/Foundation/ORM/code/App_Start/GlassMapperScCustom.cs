9d8afa935ffed579c3c0a62b561a16a75e956ed0
tarnok.tamas@allwin.hu <tarnoktomi@gmail.com>
2018-04-19 16:37:57 +0200

    Glass Mapper DI fix

--- src/Foundation/ORM/code/App_Start/GlassMapperScCustom.cs
+++ src/Foundation/ORM/code/App_Start/GlassMapperScCustom.cs
@@ -20,6 +20,8 @@ namespace Helix.Skeleton.Foundation.ORM.App_Start
 
             // add any changes to the standard resolver here
 
+            dependencyResolver.Finalize();
+
             return dependencyResolver;
 		}
 
