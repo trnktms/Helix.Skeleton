3076c529226119185e7f410673a64da38bdc6d84
tarnok.tamas@allwin.hu <tarnoktomi@gmail.com>
2018-06-13 17:28:12 +0200

    build.cmd solution path fix

--- sk-templates/default/build/build.cmd
+++ sk-templates/default/build/build.cmd
@@ -1,3 +1,3 @@
 SET _publishProfile=%cd%\LOCAL.pubxml
-SET _solution=%cd:~0,-5%src\MyProject.Website.sln
+SET _solution=%cd:~0,-5%src\[projectName].Website.sln
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %_solution% /p:DeployOnBuild=true /p:PublishProfile=%_publishProfile% /p:VisualStudioVersion=14.0
