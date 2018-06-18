136e9014cdca2fdd7368fa36928c4a82d31fc69e
tarnok.tamas@allwin.hu <tarnoktomi@gmail.com>
2018-06-18 15:31:50 +0200

    Create foundation template

---
+++ sk-templates/new-project/default/foundation/src/Foundation/[subProjectName]/code/DI/RegisterContainer.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace [projectName].Foundation.[subProjectName].DI
+{
+    public class RegisterContainer : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+        }
+    }
+}
